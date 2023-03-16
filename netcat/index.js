ad8f5c4a15767633b3ec37b0775df19e3e2393f5
Josh Endries <josh@endries.org>
2023-03-16 12:02:45 -0700

    Update to latest discord.js lib and cleanup

--- netcat/index.js
+++ netcat/index.js
@@ -1,27 +1,23 @@
-const v = '0.2';
+const v = '0.3';
 const Discord = require('discord.js');
 const Tail = require('tail');
-const Fs = require('fs');
 
 const configFilePath = process.argv[2];
-const config = JSON.parse(Fs.readfileSync(configFilePath));
-const guildChatChannelId = config.guildChatChannelId;
-const logFilePath = config.logFilePath;
-const botToken = config.botToken;
+const config = require(configFilePath);
+const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
-const client = new Discord.Client;
 let testChannel = null;
 
 client.on('ready', () => {
     if (testChannel == null) {
-        testChannel = client.channels.get(guildChatChannelId);
+        testChannel = client.channels.cache.get(config.guildChatChannelId);
         testChannel.send(`online v${v}`);
     }
 });
 
-client.login(botToken);
+client.login(config.botToken);
 
-const tail = new Tail.Tail(logFilePath);
+const tail = new Tail.Tail(config.logFilePath);
 tail.on('line', data => {
     if (testChannel != null && data.indexOf('tells the guild') > 0) {
         testChannel.send(data.substring(27).replace(' tells the guild', '').trim());
